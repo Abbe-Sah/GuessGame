648d801110c84f727fc1c3c91f7465cc1f83406d
pablo-chacon <Snemilio@protonmail.com>
2022-12-06 21:57:23 +0100

    trivia.csv + object Query

--- GuessGame/src/main/java/trivia/Trivia.java
+++ GuessGame/src/main/java/trivia/Trivia.java
@@ -6,11 +6,12 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Trivia  {
 
     static Query query = new Query();
-    static List<String> queries = new ArrayList<>(2);
+    static List<Query> queries = new ArrayList<>(20);
 
     public static void loadTrivia() throws IOException {
         String line;
@@ -25,8 +26,13 @@ public class Trivia  {
             query.setMock1("Mock");
             query.setMock2("Mock");
             query.setMock3("Mock");
-            queries.add(query.toString());
+            queries.add(query);
         }
+    }
+
+    public static String randomQuery() {
+        int r = new Random().nextInt(queries.size());
+        return String.valueOf(queries.get(r));
     }
 
 }
