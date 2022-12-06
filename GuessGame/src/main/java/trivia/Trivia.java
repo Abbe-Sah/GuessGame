493909eeb33e154c2a38a139fe2bde3238da5fdb
pablo-chacon <Snemilio@protonmail.com>
2022-12-06 22:47:29 +0100

    trivia.csv + object Query

--- GuessGame/src/main/java/trivia/Trivia.java
+++ GuessGame/src/main/java/trivia/Trivia.java
@@ -4,6 +4,8 @@ package trivia;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -11,28 +13,41 @@ import java.util.Random;
 public class Trivia  {
 
     static Query query = new Query();
-    static List<Query> queries = new ArrayList<>(20);
+    static List<Query> queries = new ArrayList<>();
+
 
     public static void loadTrivia() throws IOException {
         String line;
         BufferedReader br = new BufferedReader(new FileReader(
-                "/home/paraply/IdeaProjects/GuessGame/GuessGame/src/main/resources/com/guessgameschool/guessgame/trivia.csv"));
+                "/resources/com/guessgameschool/guessgame/trivia.csv"));
+
         while ((line = br.readLine()) != null) {
             // seprator ",".
             String[] cols = line.split(",");
             query.setSubject(cols[0]);
             query.setQuery(cols[1]);
             query.setCorrectAnswer(cols[2]);
-            query.setMock1("Mock");
-            query.setMock2("Mock");
-            query.setMock3("Mock");
             queries.add(query);
         }
     }
 
     public static String randomQuery() {
-        int r = new Random().nextInt(queries.size());
-        return String.valueOf(queries.get(r));
+        List<String> lines;
+        try {
+            lines = Files.readAllLines(
+                    Path.of("/resources/com/guessgameschool/guessgame/trivia.csv"));
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+        Random random = new Random();
+        return lines.get(random.nextInt(lines.size()));
     }
 
+    public static void main(String[] args) throws IOException {
+        loadTrivia();
+        System.out.println(randomQuery());
+
+    }
 }
